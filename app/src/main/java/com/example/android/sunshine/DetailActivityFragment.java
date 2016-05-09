6df47b44ceaeb4a2b380bbd8aa9b41f0c2714049
mmehta <madhur.eng@gmail.com>
2016-05-09 00:00:38 -0700

    Lesson 3 completed

--- app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
@@ -3,8 +3,13 @@ package com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,8 +20,11 @@ import android.widget.TextView;
 public class DetailActivityFragment extends Fragment {
 
     private final String LOG_TAG = getClass().getName();
+    private static final String SHARE_HASH_TAG = " #SunshineApp";
+    private String mForecastSelected;
 
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -25,7 +33,7 @@ public class DetailActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String mForecastSelected = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastSelected = intent.getStringExtra(Intent.EXTRA_TEXT);
             Log.d(LOG_TAG, mForecastSelected);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
             textView.setText(mForecastSelected);
@@ -33,4 +41,31 @@ public class DetailActivityFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate menu resource file.
+        inflater.inflate(R.menu.detail_fragment, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastSelected + SHARE_HASH_TAG);
+        return shareIntent;
+    }
 }
